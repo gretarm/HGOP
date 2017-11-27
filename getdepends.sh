d2ba42cd4a8be28cfa440688ea63ce0172396b79
gretar <gretarm15@ru.is>
2017-11-27 11:08:11 +0000

    Script now prints current linux distro

--- getdepends.sh
+++ getdepends.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# this is a comment
+# Prints currents users
 echo Hello $USER 
-echo System information:
-lsb_release -a
+#Gets and prints current enviroment. Not tested on other than Ubuntu...
+echo Current distro: $(grep -e "DISTRIB_DESCRIPTION" /etc/*-release | grep -oP '"\K[^"\047]+(?=["\047])')
 read -p "Do you want to use this script to get the required dependecies? y/n " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
